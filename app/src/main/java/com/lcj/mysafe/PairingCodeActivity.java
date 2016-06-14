3ad68ba32660f7c9e03e2236b0292de0cab8e136
ke <1757730121@qq.com>
2016-06-14 23:54:54 +0800

    添加activity_pairingcode布局,添加ParingCodeActivity,修改前两个布局中edittext中的右侧图片

---
+++ app/src/main/java/com/lcj/mysafe/PairingCodeActivity.java
@@ -0,0 +1,15 @@
+package com.lcj.mysafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ke on 2016/6/14.
+ */
+public class PairingCodeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pairingcode);
+    }
+}
